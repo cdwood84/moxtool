c343bc365cedba5c2aad64a9faed6e40ed959cff
Charles Wood <cdwood84@gmail.com>
2025-03-11 12:31:12 -0400

    basic models to migrate

--- moxtoolsite/catalog/models.py
+++ moxtoolsite/catalog/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+
+class Genre(models.Model):
+    """Model representing a dance music genre."""
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a dance music genre (e.g. Progressive House, Future Bass, etc.)"
+    )
+
+    def __str__(self):
+        """Function returning a string of the genre name."""
+        return self.name
+    
+    def get_absolute_url(self):
+        """Function returning a URL for genre details."""
+        return reverse('genre-detail', args=[str(self.id)])
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='genre_name_case_insensitive_unique',
+                violation_error_message="Genre already exists (case insensiitive match)"
+            ),
+        ]
+
+
+class Track(models.Model):
+    """Model representing a music track, not specifically in any user's library."""
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey('Artist', on_delete=models.RESTRICT, null=True)
+    beatport_track_id = models.BigIntegerField('BeatportTrack', unique=True, help_text='Track ID from Beatport, found in the track URL, which can be used to populate metadata.')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this track")
+
+    def __str__(self):
+        """Function returning a string of the track title."""
+        return self.title
+    
+    def get_absolute_url(self):
+        """Function returning a URL for track details."""
+        return reverse('track-detail', args=[str(self.id)])
+    
+
+class TrackInstance(models.Model):
+    """Model representing a music track in a specific user library."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this track and owner library")
+    track = models.ForeignKey('Track', on_delete=models.RESTRICT, null=True)
+    comments = models.TextField(max_length=1000, help_text = "Enter any notes you want to remember about this track")
+    date_added = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        """Function returning a string of the track title."""
+        return f'{self.id} ({self.track.title})'
+
+    class Meta:
+        ordering = ['date_added']
+    
+
+class Artist(models.Model):
+    """Model representing a musical artist."""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """Function returning a string of the artist name."""
+        return self.name
+
+    def get_absolute_url(self):
+        """Function returning a URL for artist details."""
+        return reverse('artist-detail', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['name']
