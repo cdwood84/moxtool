1e69a0996cc7e5c2c6665d8399c4de1b65cd4422
Charles Wood <cdwood84@gmail.com>
2025-04-03 09:54:54 -0400

    add print for debug context

--- moxtoolsite/moxtoolsite/settings.py
+++ moxtoolsite/moxtoolsite/settings.py
@@ -6,6 +6,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
 load_dotenv(env_path)
 
