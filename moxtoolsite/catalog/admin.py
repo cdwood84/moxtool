e020a16a01a68ee0c4440ac24f4199a13146c7ec
Charles Wood <cdwood84@gmail.com>
2025-04-03 12:43:29 -0400

    remove tags from playlist admin

--- moxtoolsite/catalog/admin.py
+++ moxtoolsite/catalog/admin.py
@@ -19,13 +19,8 @@ class GenreAdmin(admin.ModelAdmin):
 
 @admin.register(Playlist)
 class PlaylistAdmin(admin.ModelAdmin):
-    list_display = ['name', 'user', 'date_added', 'display_tags']
+    list_display = ['name', 'user', 'date_added']
     list_filter = ['user', 'date_added', 'tag']
-
-    def display_tags(self, obj):
-        return obj.tag.display()
-    
-    display_tags.short_description = 'Tags'
 
 
 @admin.register(Tag)
