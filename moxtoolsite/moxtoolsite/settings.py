255a208c04bc70230d4ab925555b85db287b6cc9
Charles Wood <cdwood84@gmail.com>
2025-04-03 10:19:37 -0400

    add CSRF

--- moxtoolsite/moxtoolsite/settings.py
+++ moxtoolsite/moxtoolsite/settings.py
@@ -134,3 +134,7 @@ if 'DATABASE_URL' in os.environ:
         conn_max_age=500,
         conn_health_checks=True,
     )
+
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://enterthemox.pythonanywhere.com']
