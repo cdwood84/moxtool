22d736690a598c98f8feedfca07d4572b7c10a40
Charles Wood <cdwood84@gmail.com>
2025-05-20 15:39:13 -0400

    use env variable to decide to use a proxy

--- moxtoolsite/catalog/utils.py
+++ moxtoolsite/catalog/utils.py
@@ -8,20 +8,25 @@ import os, random, requests, string, time
 
 
 def get_soup(url, iteration_count=0):
-    print(' proxy'+str(os.environ.get('MY_PROXY_LIST')))
-    proxies = os.environ.get('MY_PROXY_LIST').split(',')
-    proxy = 'http://' + os.environ.get('MY_PROXY_CREDS') + '@' + random.choice(proxies)
-    user_agents = os.environ.get('MY_USER_AGENT_LIST').split('&')
-    user_agent = random.choice(user_agents)
-    print(' ua: '+ str(user_agent))
     extra_time = iteration_count * 5
     time.sleep(random.randint(3+extra_time, 8+extra_time))
-    response = requests.get(
-        url, 
-        proxies = {'http': proxy}, 
-        headers = {'User-Agent': user_agent} , 
-        timeout = 15,
-    )
+    user_agents = os.environ.get('MY_USER_AGENT_LIST').split('&')
+    user_agent = random.choice(user_agents)
+    if os.environ.get('USE_PROXY') == True:
+        proxies = os.environ.get('MY_PROXY_LIST').split(',')
+        proxy = 'http://' + os.environ.get('MY_PROXY_CREDS') + '@' + random.choice(proxies)
+        response = requests.get(
+            url, 
+            proxies = {'http': proxy}, 
+            headers = {'User-Agent': user_agent} , 
+            timeout = 15,
+        )
+    else:
+        response = requests.get(
+            url, 
+            headers = {'User-Agent': user_agent} , 
+            timeout = 15,
+        )
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
